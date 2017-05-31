ad72745e1ef5952f31eeab91e29301fe085e0c76
Grady Shelton and Kai deRochemont <grady+kai@>
2017-05-31 11:46:21 -0700

    add form to create new post

---
+++ app/components/new-post.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewPost: false,
+  actions: {
+    postFormShow() {
+      this.set('addNewPost', true);
+    },
+    savePost() {
+      var params = {
+        title: this.get('title') ? this.get('title') : "",
+        author: this.get('author') ? this.get('author') : "",
+        date: this.get('date') ? this.get('date') : "",
+        body: this.get('body') ? this.get('body') : "",
+      }
+      this.set('addNewPost', false);
+      this.sendAction('savePost', params);
+    }
+  }
+
+});
